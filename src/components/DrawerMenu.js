325f2c6a030ce8a26deb2f92a076ea206394d4ed
caudaganesh <caudaganesh@gmail.com>
2017-11-19 18:45:55 +0700

    clean up codes - add setting placeholder

--- src/components/DrawerMenu.js
+++ src/components/DrawerMenu.js
@@ -4,18 +4,22 @@ import {
     StyleSheet,
     TouchableOpacity,
     Text,
-    Platform,
     ScrollView,
-    TextInput
+    TextInput,
+    Slider
 } from "react-native";
+import PropTypes from 'prop-types';
 import Books from "../constants/Books";
+import { COLOR, SUBMENU } from '../constants/constants';
 import Icon from "react-native-vector-icons/Ionicons";
+import Accordion from 'react-native-collapsible/Accordion';
 
 
 export default class DrawerMenu extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            headerFlex: 1
         };
     }
 
@@ -30,7 +34,45 @@ export default class DrawerMenu extends Component {
     _onJumpText = (jumpText) => {
         this.props._onJumpText(jumpText);
     }
-    
+
+    _renderSectionHeader(section) {
+        return (
+            <View style={styles.horizontalContaier}>
+                <Icon name={section.icon} size={25} color={COLOR.mainTextColor} />
+                <Text style={styles.subMenuTextTitle}>{section.title.toUpperCase()}</Text>
+            </View>
+        );
+    }
+
+    _toggleSetting(index) {
+        this.setState({
+            headerFlex: index === false ? 1 : 0
+        })
+    }
+
+    _renderSettingSection = () => {
+        const { fontValue } = this.props || 0;
+        const { onFontValueChange } = this.props || {};
+        return (
+            <View>
+                <ScrollView style={styles.verticalContainer}>
+                    <View style={styles.horizontalContaier}>
+                        <Icon name="ios-brush" size={25} color={COLOR.mainTextColor} />
+                        <Text style={styles.subMenuTextTitle}>FONT SIZE</Text>
+                    </View>
+                    <Slider
+                        maximumValue={10}
+                        value={fontValue}
+                        thumbTintColor='#fff'
+                        minimumTrackTintColor='#1fb28a'
+                        maximumTrackTintColor='#d3d3d3'
+                        onValueChange={(value) => { onFontValueChange(value) }}
+                    />
+                </ScrollView>
+            </View>
+        )
+    }
+
     _renderBook(book, i) {
         const { activeBook, activeChapter } = this.props;
         const isBookActive = activeBook.value == book.value;
@@ -46,7 +88,7 @@ export default class DrawerMenu extends Component {
         );
         if (isBookActive) {
             let chapters = [];
-            for (var i = 1; i <= book.total; i++) {
+            for (let i = 1; i <= book.total; i++) {
                 chapters.push(i);
             }
             return (
@@ -55,8 +97,7 @@ export default class DrawerMenu extends Component {
                     <View style={styles.chapterSelector}>
                         <ScrollView
                             horizontal={true}
-                            contentContainerStyle={styles.chapterScroll}
-                        >
+                            contentContainerStyle={styles.chapterScroll}>
                             {chapters.map(chapter => {
                                 const isChapterActive = chapter == activeChapter;
                                 return (
@@ -64,13 +105,10 @@ export default class DrawerMenu extends Component {
                                         activeOpacity={0.7}
                                         key={`${book.value}-${chapter}`}
                                         style={styles.chapter}
-                                        onPress={() => this._changeActiveChapter(chapter)}
-                                    >
+                                        onPress={() => this._changeActiveChapter(chapter)}>
                                         {isChapterActive ? (
                                             <View style={styles.chapterActive}>
-                                                <Text
-                                                    style={[styles.chapterText, styles.chapterTextActive]}
-                                                >
+                                                <Text style={[styles.chapterText, styles.chapterTextActive]}>
                                                     {chapter}
                                                 </Text>
                                             </View>
@@ -91,81 +129,107 @@ export default class DrawerMenu extends Component {
 
     render() {
         const { jumpText } = this.props;
+        const { headerFlex } = this.state;
         return (
             <View style={styles.drawerWrapper}>
                 <View style={styles.drawerHeader}>
-                    <TouchableOpacity activeOpacity={0.7} style={styles.drawerVersion}>
-                        <Text style={styles.versionText}>Terjemahan Baru</Text>
-                        <Icon name="ios-book" size={25} color="#fff" />
-                    </TouchableOpacity>
-                    <TextInput
-                        placeholder={"Jump here"}
-                        placeholderTextColor={"rgba(255,255,255,0.3)"}
-                        value={jumpText}
-                        style={styles.input}
-                        onSubmitEditing={() => this.props._onSubmitJump()}
-                        onChangeText={jumpText => this._onJumpText(jumpText)}
-                    />
-                    <TouchableOpacity
-                        activeOpacity={0.7}
-                        style={styles.clearJump}
-                        onPress={() => this.props._onClearJump()}
-                    >
-                        <Icon name="ios-close" size={30} color="#fff" />
-                    </TouchableOpacity>
+                    <View style={{ flex: headerFlex }}>
+                        <TouchableOpacity activeOpacity={0.7} style={styles.drawerVersion}>
+                            <Text style={styles.versionText}>Terjemahan Baru</Text>
+                            <Icon name="ios-book" size={25} color={COLOR.mainTextColor} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={[styles.horizontalContaier, { flex: headerFlex }]}>
+                        <TextInput
+                            placeholder={"Jump here"}
+                            placeholderTextColor={"rgba(255,255,255,0.3)"}
+                            value={jumpText}
+                            style={styles.input}
+                            onSubmitEditing={() => this.props._onSubmitJump()}
+                            onChangeText={jumpText => this._onJumpText(jumpText)}
+                        />
+                        <TouchableOpacity
+                            activeOpacity={0.7}
+                            style={styles.clearJump}
+                            onPress={() => this.props._onClearJump()}
+                        >
+                            <Icon name="ios-close" size={30} color={COLOR.mainTextColor} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ padding: 16 }}>
+                        <Accordion
+                            onChange={(index) => this._toggleSetting(index)}
+                            sections={SUBMENU}
+                            renderHeader={this._renderSectionHeader}
+                            renderContent={this._renderSettingSection}
+                        />
+                    </View>
                 </View>
-                <ScrollView>
-                    <Text style={styles.separator}>OLD TESTAMENT</Text>
-                    {Books.filter(
-                        book =>
-                            book.type == "old" &&
-                            book.name_id.toLowerCase().indexOf(jumpText.toLowerCase()) != -1
-                    ).map((book, i) => {
-                        return this._renderBook(book, i);
-                    })}
-                    <Text style={styles.separator}>NEW TESTAMENT</Text>
-                    {Books.filter(
-                        book =>
-                            book.type == "new" &&
-                            book.name_id.toLowerCase().indexOf(jumpText.toLowerCase()) != -1
-                    ).map((book, i) => {
-                        return this._renderBook(book, i);
-                    })}
-                </ScrollView>
+
+                <View style={styles.darkOverlay}>
+                    <ScrollView >
+                        <Text style={styles.separator}>OLD TESTAMENT</Text>
+                        {Books.filter(
+                            book =>
+                                book.type == "old" &&
+                                book.name_id.toLowerCase().indexOf(jumpText.toLowerCase()) != -1
+                        ).map((book, i) => {
+                            return this._renderBook(book, i);
+                        })}
+                        <Text style={styles.separator}>NEW TESTAMENT</Text>
+                        {Books.filter(
+                            book =>
+                                book.type == "new" &&
+                                book.name_id.toLowerCase().indexOf(jumpText.toLowerCase()) != -1
+                        ).map((book, i) => {
+                            return this._renderBook(book, i);
+                        })}
+                    </ScrollView>
+                </View>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-   
-    
     drawerWrapper: {
-        backgroundColor: "#1f364d",
+        flexDirection: 'column',
+        backgroundColor: COLOR.primary,
         flex: 1,
         paddingTop: 20
     },
+    horizontalContaier: {
+        flexDirection: 'row'
+    },
+    verticalContainer: {
+        flexDirection: 'column',
+        marginLeft: 24
+    },
     separator: {
         fontWeight: "900",
-        color: "rgba(255,255,255,0.4)",
+        color: COLOR.mainSeparator,
         fontSize: 10,
         marginVertical: 10,
         marginLeft: 20
+    },
+    darkOverlay: {
+        flex: 1,
+        backgroundColor: COLOR.darkOverlay
     },
     book: {
         paddingHorizontal: 25,
         paddingVertical: 20
     },
     bookText: {
-        color: "#fff",
+        color: COLOR.mainTextColor,
         fontWeight: "300"
     },
     bookActive: {
-        backgroundColor: "#26405A"
+        backgroundColor: COLOR.primaryDark
     },
     chapterSelector: {
         height: 60,
-        backgroundColor: "#26405A"
+        backgroundColor: COLOR.primaryDark
     },
     chapter: {
         height: 60,
@@ -174,30 +238,29 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     chapterText: {
-        color: "#fff"
+        color: COLOR.mainTextColor
     },
     chapterActive: {
         width: 40,
         height: 40,
         borderRadius: 20,
-        backgroundColor: "#fff",
+        backgroundColor: COLOR.activeBackgroundColor,
         justifyContent: "center",
         alignItems: "center"
     },
     chapterTextActive: {
-        color: "#0D233A"
+        color: COLOR.primaryExtraDark
     },
     chapterScroll: {
         paddingHorizontal: 20
     },
-    verseNumber: {
-        fontWeight: "900",
-        paddingRight: 20,
-        color: "#26405A",
-        fontSize: 10
-    },
     drawerHeader: {
-        height: 100
+        height: 200
+    },
+    subMenuTextTitle: {
+        color: COLOR.mainTextColor,
+        marginLeft: 12,
+        alignSelf: 'center'
     },
     drawerVersion: {
         flexDirection: "row",
@@ -207,27 +270,34 @@ const styles = StyleSheet.create({
         flex: 1
     },
     versionText: {
-        color: "#fff",
+        color: COLOR.mainTextColor,
         flex: 1
     },
     input: {
         flex: 1,
-        height: 20,
-        backgroundColor: "#26405A",
+        backgroundColor: COLOR.primaryDark,
         margin: 5,
         marginBottom: 10,
         marginHorizontal: 10,
         borderRadius: 7,
         paddingHorizontal: 10,
-        color: "#fff",
+        color: COLOR.mainTextColor,
         fontSize: 13
     },
     clearJump: {
-        position: "absolute",
-        right: 10,
-        top: 55,
         height: 30,
         width: 30,
-        backgroundColor: "transparent"
+        backgroundColor: COLOR.clearBackground
     }
 });
+
+DrawerMenu.propTypes = {
+    _changeActiveBook: PropTypes.func.isRequired,
+    _changeActiveChapter: PropTypes.func.isRequired,
+    _onClearJump: PropTypes.func.isRequired,
+    _onJumpText: PropTypes.func.isRequired,
+    _onSubmitJump: PropTypes.func.isRequired,
+    activeBook: PropTypes.object.isRequired,
+    activeChapter: PropTypes.number.isRequired,
+    jumpText: PropTypes.string.isRequired
+}
